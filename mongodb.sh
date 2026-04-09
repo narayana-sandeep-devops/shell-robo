e118ce16eff7e37222e7f19e6d66684d14231961
Sandeep Narayana <sandeep.narayana2013@outlook.com>
2026-04-09 18:30:17 +0530

    new package created

---
+++ mongodb.sh
@@ -0,0 +1,51 @@
+#!/bin/bsh
+
+
+
+
+echo "$R Hello, $G Iam Green $Y Learning $N DEVOPS"
+
+
+USERID=$(id -u)
+LOG_FOLDER="/var/log/shell-roboshop"
+LOG_FILE="$LOG_FOLDER/$0.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this command with root user: $N" | tee -a $LOG_FILE
+    exit 1
+fi
+
+mkdir -p $LOG_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2: $R Failure $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2: $G Success $Y" | tee -a $LOG_FILE
+    fi
+}
+
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying Mongo Repo"
+
+dnf isntall mongodb-org -y
+VALIDATE $? "Installing MongoDB Server"
+
+systemctl enable mongodb
+VALIDATE $? "Enable MongoDB"
+
+Systemctl start mongodb
+VALIDATE $? "MongoDB started"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections"
+
+system restart mongod
+VALIDATE $? "MongoDB Restarted"
