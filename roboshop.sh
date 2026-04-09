bb978f9032a7792a7c66283c073132b0ba40e868
Sandeep Narayana <sandeep.narayana2013@outlook.com>
2026-04-09 10:53:39 +0530

    new package created

--- roboshop.sh
+++ roboshop.sh
@@ -2,7 +2,8 @@
 
 SG_ID="sg-0c53fd79d4d2aa973"
 AMI_ID="ami-0220d79f3f480ecf5"
-
+ZONE_ID="Z02709693P5LDKOA75JSO"
+DOMAIN_NAME="sandeepinfo.online"
 for instance in $@
 do
     INSTANCE_ID=$( aws ec2 run-instances \
@@ -19,7 +20,7 @@ do
                 --instance-ids $INSTANCE_ID \
                 --query 'Reservations[].Instances[].PublicIpAddress' \
                 --output text
-            )
+            )            
         else
             IP=$(
             aws ec2 describe-instances \
@@ -27,11 +28,30 @@ do
                 --query 'Reservations[].Instances[].PrivateIpAddress' \
                 --output text
             )
+            RECORD_NAME="$instance.$DOMAIN_NAME" #ex:mongo
         fi
         echo "IP Address: $IP"
+
+        aws route53 list-resource-record-sets \
+        --hosted-zone-id <YOUR_HOSTED_ZONE_ID> \
+        --change-batch '
+            {
+                "Comment": "Optional comment",
+                "Changes": [
+                    {
+                        "Action": "UPSERT",
+                        "ResourceRecordSet": {
+                            "Name": "'$RECORD_NAME'",
+                            "Type": "A",
+                            "TTL": 1,
+                            "ResourceRecords": [
+                                { "Value": "$IP" }
+                            ]
+                        }
+                    }
+                ]
+            }
+            echo "Record updated for isntance: $instance"
 done
-
-
-
 
 
